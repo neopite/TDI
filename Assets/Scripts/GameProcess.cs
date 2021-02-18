2c7ea3453f3270a1bd5f8600ab681199df5b9a5a
Yaroslav <Stamin4ik4@gmail.com>
2021-02-18 14:24:15 +0200

    implemented step by step enemy movement

--- Assets/Scripts/GameProcess.cs
+++ Assets/Scripts/GameProcess.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DefaultNamespace.Enemy;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DefaultNamespace
 {
@@ -13,6 +17,9 @@ namespace DefaultNamespace
         private int _wavesSpawned;
         private Dictionary<int, List<EnemyBase>> _wavesPosition;
         private List<EnemyCell> _tiles;
+        public bool isLevelStarted;
+        public TextMeshProUGUI GameState;
+
         private void Start()
         {
             _enemyGridManager = EnemyGridManager.Instance;
@@ -21,42 +28,74 @@ namespace DefaultNamespace
             _levelWaves = _enemyGridManager.Waves;
             _wavesPosition = new Dictionary<int, List<EnemyBase>>();
             SpawnWave();
-            MoveWave(0);
-            MoveWave(1);
-            SpawnWave();
-            MoveWave(0);
-            MoveWave(2);
         }
 
         private void Update()
         {
+            if (isLevelStarted)
+            {
+                List<int> wavesId = _wavesPosition.Keys.ToList();
+                for (int i = 0 ;  i < wavesId.Count ; i++)
+                {
+                    MoveWave(wavesId[i]);
+                }
+                SpawnWave();
+                isLevelStarted = false;
+            }
         }
 
         private void SpawnWave()
         {
-            List<EnemyBase> listOfEnemies = new List<EnemyBase>();
-            for (int i = 0; i < _levelWaves[_wavesSpawned].ListOfEnemies.Count; i++)
+            if (_levelWaves.Count == _wavesSpawned)
             {
-                EnemyBase gm = Instantiate(_levelWaves[_wavesSpawned].ListOfEnemies[i],
+                Debug.Log("Waves Spanw ended");
+            }
+            else
+            {
+                List<EnemyBase> listOfEnemies = new List<EnemyBase>(); 
+                for (int i = 0; i < _levelWaves[_wavesSpawned].ListOfEnemies.Count; i++) 
+                { 
+                    EnemyBase gm = Instantiate(_levelWaves[_wavesSpawned].ListOfEnemies[i],
                     transform.parent);
                 gm.transform.position = EnemyGridManager.Instance._previewEnemyCells[i].transform.position;
                 listOfEnemies.Add(gm);
-            }
-            _wavesPosition.Add(0,listOfEnemies);
+                } 
+                _wavesPosition.Add(0, listOfEnemies);
 
-            _wavesSpawned++;
+                 _wavesSpawned++;
+                 
+            }
         }
 
         private void MoveWave(int waveId)
         {
-            for (int i = 0; i < _wavesPosition[waveId].Count; i++)
+            if (waveId == _enemyGridManager._enemyGrid.Rows)
             {
-                    _wavesPosition[waveId][i].ChangeStage(_tiles[waveId*_enemyGridManager._enemyGrid.Columns+i].transform.position);
+                Debug.Log("Game over");
+                isLevelStarted = false;
             }
+            else
+                for (int i = 0; i < _wavesPosition[waveId].Count; i++)
+                {
+                    _wavesPosition[waveId][i]
+                        .ChangeStage(_tiles[waveId * _enemyGridManager._enemyGrid.Columns + i].transform.position);
+                }
+
             List<EnemyBase> wave = _wavesPosition[waveId];
             _wavesPosition.Remove(waveId);
-            _wavesPosition.Add(++waveId,wave);
+            _wavesPosition[++waveId] = wave;
+        }
+    
+
+    public void ChangeLevelState()
+        {
+            isLevelStarted = !isLevelStarted;
+            GameState.text = isLevelStarted ? "Pause" : "Continue";
         }
 
+        private IEnumerator Delay(float time)
+        {
+            yield return new WaitForSeconds(time);
+        }
     }
 }
